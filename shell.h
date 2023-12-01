f25f8ca7350b49161f02b4f0071ef1e24a282fb2
kawthar <kawtharikram2020@gmail.com>
2023-12-01 11:26:31 -0800

    shell

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+
+
+void _print_prompt(void);
+
+#endif
