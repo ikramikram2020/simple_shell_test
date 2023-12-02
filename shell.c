f7506fbfaed997dda7f91f6c6d5079faa62fb6ce
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2023-12-02 06:56:31 -0800

    Fix betty style

--- shell.c
+++ shell.c
@@ -1,5 +1,14 @@
 #include "shell.h"
 
+/**
+ * main - main shell to execute commands
+ *
+ * @argc: number of arguments
+ * @argv: array of strings of arguments
+ *
+ * Return: 0 on success, -1 on failure
+ */
+
 int main(int argc, char **argv)
 {
 	(void)argc, (void)argv;
